35c08b6eabb1f3050993d8b8fd051ba8d4ece2af
Eduardo Piorini <eduardopiorini@hotmail.com>
2020-12-04 06:17:04 -0300

    handling files upload - start of

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -14,9 +17,11 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 
+const upload = multer(uploadConfig);
+
 usersRouter.post('/', async (request, response) => {
     try {
-        const {name, email, password} = request.body;
+        const { name, email, password } = request.body;
 
         const createUser = new CreateUserService();
 
@@ -25,18 +30,21 @@ usersRouter.post('/', async (request, response) => {
             email,
             password,
         });
-        
+
         delete user.password;
 
         return response.json(user);
-    
+
     } catch (err) {
         return response.status(400).json({ error: err.message });
     }
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-    return response.json({ok: true});
+usersRouter.patch('/avatar', ensureAuthenticated, 
+upload.single('avatar'),
+async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
 })
 
 export default usersRouter;
